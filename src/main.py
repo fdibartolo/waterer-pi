464a1b84b0751ad84cb355cadd77e77b7ee5b21f
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2018-09-28 14:42:03 -0300

    env var

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from os import environ
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Waterer!'
+
+  p = environ.get('PASS')
+  print p
+  return 'Welcome to Waterer!' + p
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+  app.run(host='0.0.0.0', port=80)
