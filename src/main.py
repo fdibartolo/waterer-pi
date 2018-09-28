4d2a2ad054a4be864a48d8ad95e70fe5f6dc21f0
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2018-09-28 16:40:13 -0300

    add button and led via gpio

--- src/main.py
+++ src/main.py
@@ -2,24 +2,29 @@ from flask import Flask
 from os import environ
 import time
 import atexit
+import waterer
 from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
 
 def schedule():
-  print('tick...')
+  if waterer.button_pressed?:
+    print('pressed!')
 
 @app.route('/')
 def hello_world():
-
+  waterer.toggle_led()
   p = environ.get('PASS')
   print(p)
   return 'Welcome to Waterer!' + p
 
-# Shut down the scheduler when exiting the app
+# Shut down the scheduler & gpio when exiting the app
 atexit.register(lambda: scheduler.shutdown())
+atexit.register(lambda: waterer.shutdown())
 
 if __name__ == '__main__':
+  waterer.setup()
+  waterer.init()
   scheduler = BackgroundScheduler()
-  scheduler.add_job(schedule, 'interval', seconds=3)
+  scheduler.add_job(schedule, 'interval', seconds=1)
   scheduler.start()
   app.run(host='0.0.0.0', port=80)
---
+++ src/waterer.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+import datetime
+
+def setup():
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(3, GPIO.IN) # button
+  GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW) # led
+
+def init():
+  print("WATERER::initializing...")
+  now = datetime.datetime.now()
+  while (datetime.datetime.now() - now).seconds < 3:
+    toggle_led()
+  print("WATERER::ready!")
+
+def toggle_led():
+  GPIO.output(11, GPIO.HIGH)
+  time.sleep(0.5)
+  GPIO.output(11, GPIO.LOW)
+  time.sleep(0.5)
+
+def shutdown():
+  GPIO.cleanup()
+  print("WATERER::goodbye!")
+
+def button_pressed?():
+  not GPIO.input(3)
