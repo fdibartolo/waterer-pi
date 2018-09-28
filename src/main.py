059ae22fd83fd0b4e7443af11b99a8b687a11d93
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2018-09-28 15:57:06 -0300

    add scheduler

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from os import environ
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
+
+def schedule():
+  print('tick...')
 
 @app.route('/')
 def hello_world():
@@ -9,5 +15,11 @@ def hello_world():
   print(p)
   return 'Welcome to Waterer!' + p
 
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
 if __name__ == '__main__':
+  scheduler = BackgroundScheduler()
+  scheduler.add_job(schedule, 'interval', seconds=3)
+  scheduler.start()
   app.run(host='0.0.0.0', port=80)
