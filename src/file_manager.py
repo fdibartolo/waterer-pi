eb6e9d1d6dc209c4d8c18a1173cb1f8a7bc3df43
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2018-09-30 14:48:39 -0300

    add file manager

---
+++ src/file_manager.py
@@ -0,0 +1,13 @@
+import datetime
+
+file_path = '/data/last_run.txt'
+
+def write(source):
+  timestamp = datetime.datetime.now()
+  f = open(file_path, 'w')
+  f.write('Triggered via ' + source + ' on ' + timestamp.strftime('%b %d') + ' at ' + timestamp.strftime('%H:%M') + ' hs')
+  f.close
+
+def read():
+  f = open(file_path, 'r')
+  return f.readline()
