084774ddae12860fb2ef1ef420cc7289b88f5c78
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2018-10-01 14:40:06 -0300

    keep last 3 events in log file

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,15 +1,29 @@
 import datetime
+import os.path
 
 file_path = '/data/last_run.txt'
 
 def write(source):
   timestamp = datetime.datetime.now()
+  line = 'Triggered via ' + source + ' on ' + timestamp.strftime('%b %d') + ' at ' + timestamp.strftime('%H:%M') + ' hs\n'
+
+  if not os.path.isfile(file_path):
+    open(file_path, 'w').close()
+
+  f = open(file_path, 'r')
+  lines = f.readlines()
+  f.close()
+
+  if len(lines) >= 3:
+    lines.pop(len(lines)-1)    
+
+  lines.insert(0, line)
   f = open(file_path, 'w')
-  f.write('Triggered via ' + source + ' on ' + timestamp.strftime('%b %d') + ' at ' + timestamp.strftime('%H:%M') + ' hs')
+  f.writelines(lines)
   f.close()
 
 def read():
   f = open(file_path, 'r')
-  content = f.readline()
+  content = f.readlines()
   f.close()
   return content
