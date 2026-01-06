4dee6f5234e4ced615a516dd4d880ebabf87556b
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2026-01-06 12:20:23 -0300

    remove status, refactor template data, init env vars

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, redirect, request
 import io
-from os import environ
+from os import environ as env
 import datetime
 import atexit
 from waterer import Waterer, WatererLocal
@@ -23,17 +23,15 @@ def auto_water():
 
 @app.route('/')
 def home():
-  lines = file_manager.read()
-  now = datetime.datetime.now()
-  templateData = { 'status' : 'Online', 'log' : lines, 'server_datetime' : now.strftime('%b %d, %H:%Mhs') }
-  if environ.get('AUTO_ENABLED') == 'True':
-    templateData.update({'auto' : True, 'time' : environ.get('HOUR') + ':' + environ.get('MINUTE'), 'button_text' : 'Turn Off'})
-  else:
-    templateData.update({'auto' : False, 'button_text' : 'Turn On'})
-
-  templateData.update({'is_watering' : environ.get('IS_WATERING') == 'True' })
-  templateData.update({'time_area_1' : environ.get('TIME_AREA_1'), 'time_area_2' : environ.get('TIME_AREA_2')})
-
+  templateData = {
+    'log' : file_manager.read(),
+    'server_datetime' : datetime.datetime.now().strftime('%b %d, %H:%Mhs'),
+    'is_watering' : env.get('IS_WATERING') == 'True',
+    'auto' : env.get('AUTO_ENABLED') == 'True',
+    'time' : f"{env.get('HOUR')}:{env.get('MINUTE')}",
+    'time_area_1' : env.get('TIME_AREA_1'),
+    'time_area_2' : env.get('TIME_AREA_2')
+  }
   return render_template('home.html', **templateData)
 
 @app.route('/healthcheck')
@@ -43,8 +41,7 @@ def health_check():
 
 @app.route('/get_datetime')
 def get_datetime():
-  now = datetime.datetime.now()
-  return jsonify({'server_datetime' : now.strftime('%b %d, %H:%Mhs')})
+  return jsonify({'server_datetime' : datetime.datetime.now().strftime('%b %d, %H:%Mhs')})
 
 @app.route('/water')
 def water():
@@ -54,24 +51,24 @@ def water():
 
 @app.route('/stop')
 def stop_watering():
-  environ['IS_WATERING'] = 'False'
+  env['IS_WATERING'] = 'False'
   print("Stop IS_WATERING = False")
   return redirect('/', code=302)
 
 @app.route('/toggle_auto')
 def toggle_auto():
-  if environ.get('AUTO_ENABLED') == 'True':
-    environ['AUTO_ENABLED'] = 'False'
+  if env.get('AUTO_ENABLED') == 'True':
+    env['AUTO_ENABLED'] = 'False'
   else:
-    environ['AUTO_ENABLED'] = 'True'
+    env['AUTO_ENABLED'] = 'True'
   return redirect('/', code=302)
 
 @app.route('/set_areas_time', methods=['POST'])
 def set_areas_time():
   print('setting area 1 watering time to ' + str(request.form['timeArea1']) + ' seconds')
   print('setting area 2 watering time to ' + str(request.form['timeArea2']) + ' seconds')
-  environ['TIME_AREA_1'] = request.form['timeArea1']
-  environ['TIME_AREA_2'] = request.form['timeArea2']
+  env['TIME_AREA_1'] = request.form['timeArea1']
+  env['TIME_AREA_2'] = request.form['timeArea2']
   return redirect('/', code=302)
 
 # Shut down the scheduler & gpio when exiting the app
@@ -86,19 +83,36 @@ def is_raspberrypi():
   except Exception:
     pass
   return False
+
+def init_envvars():
+  env['IS_WATERING'] = 'False'
   
+  if env.get('AUTO_ENABLED') is None:
+    env['AUTO_ENABLED'] = 'False'
+    
+  if env.get('HOUR') is None:
+    env['HOUR'] = '00'
+    
+  if env.get('MINUTE') is None:
+    env['MINUTE'] = '00'
+    
+  if env.get('TIME_AREA_1') is None:
+    env['TIME_AREA_1'] = '0'
+    
+  if env.get('TIME_AREA_2') is None:
+    env['TIME_AREA_2'] = '0'
+    
 if __name__ == '__main__':
   file_manager = FileManager('./last_run.txt')
   waterer = Waterer(file_manager) if is_raspberrypi() else WatererLocal(file_manager)
   scheduler = BackgroundScheduler()
   scheduler.add_job(schedule, 'interval', seconds=1)
 
-  environ['IS_WATERING'] = 'False'
-  print("Init IS_WATERING = False")
+  init_envvars()
 
-  if environ.get('AUTO_ENABLED') == 'True':
-    hh = environ.get('HOUR')
-    mm = environ.get('MINUTE')
+  if env.get('AUTO_ENABLED') == 'True':
+    hh = env.get('HOUR')
+    mm = env.get('MINUTE')
     print('setting scheduler for automatic watering to ' + hh + ':' + mm + 'hs (UTC)')
     scheduler.add_job(auto_water, 'cron', day_of_week='mon-sun', hour=int(hh), minute=int(mm))
 
