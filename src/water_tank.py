2e9109674c3e2258ceff12f7e49d18df54169612
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2026-01-14 14:58:41 -0300

    convert distance to tank level percentage

--- src/water_tank.py
+++ src/water_tank.py
@@ -9,6 +9,9 @@ class WaterTank:
   ULTRASONIC_TRIGGER = 15
   ULTRASONIC_ECHO = 16
   
+  SENSOR_OFFSET = 12  # distance from sensor to tank top level (when 100% full) in cm
+  EMPTY_DISTANCE = 107  # distance from sensor to tank bottom level (when considered empty) in cm
+  
   def __init__(self):
     print(f"WATERTANK::starting as {type(self).__name__} mode...")
 
@@ -24,6 +27,15 @@ class WaterTank:
     GPIO.cleanup()
     print("WATERTANK::goodbye!")
 
+  def __to_percentage(self, dist):
+    if dist <= self.SENSOR_OFFSET:
+      return 100
+    elif dist >= self.EMPTY_DISTANCE:
+      return 0
+    else:
+      percentage = 100 - ((dist - self.SENSOR_OFFSET)/(self.EMPTY_DISTANCE - self.SENSOR_OFFSET)*100)
+      return round(percentage, 0)
+    
   def measure_distance(self):
     GPIO.output(WaterTank.ULTRASONIC_TRIGGER, GPIO.HIGH)
     time.sleep(0.00001)
@@ -36,8 +48,8 @@ class WaterTank:
       pulse_end = time.time()
       
     pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150  # in cm
-    return round(distance, 2)
+    dist = pulse_duration * 17150  # in cm
+    return self.__to_percentage(dist)
       
 class WaterTankLocal:
   def __init__(self):
